3b483688af81b38e257f9c42052442472461212a
project-webapp360 <webapp360@yandex.ru>
2022-04-06 21:54:48 +0300

    fetch changes

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -6,8 +6,8 @@ const roleMiddleware = require('../middlewares/roleMiddleware')
 const path = require("path");
 
 
-router.post('/register', userController.registration )
-router.post('/login', userController.login )
+router.post('/register', userController.registration)
+router.post('/login', userController.login)
 router.get('/users', roleMiddleware('ADMIN'), userController.getAllUsers)
 router.get('/token/refresh', userController.refreshToken)
 router.post('/token/delete', userController.deleteToken)
